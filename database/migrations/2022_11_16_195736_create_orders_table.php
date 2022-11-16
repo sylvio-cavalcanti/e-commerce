e89f3b519752864b977e89ed96b7715b915dbf06
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-16 18:50:43 -0300

    Feat: creating the other migrations for the application

--- database/migrations/2022_11_16_195736_create_orders_table.php
+++ database/migrations/2022_11_16_195736_create_orders_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User; // used in the foreignIdFor 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total_price', 20, 2);
+            $table->string('status', 45);
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
             $table->timestamps();
         });
     }
