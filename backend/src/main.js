8862c36edad38c00591a2696a9c6f6e9a38064db
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-16 22:23:16 -0300

    Installed Vuex

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
-import './index.css'
+import './index.css' // Tailwind
+import store from './store' // Vuex
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
