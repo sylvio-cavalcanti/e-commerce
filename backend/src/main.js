cee37b839b6cd9e36f508f9dcb056cab9f6ea62c
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-16 22:59:57 -0300

    Installed Vue-Router, and created the routes and components for Login and Dashboard

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import './index.css' // Tailwind
 import store from './store' // Vuex
+import router from './router' // Router
 import App from './App.vue'
 
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
---
+++ backend/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Login Form</h1>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    
+    mounted() {
+        console.log('LOGIN...');
+    }
+}
+</script>
+
+<style>
+
+</style>
