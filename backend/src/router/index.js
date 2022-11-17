e1bc957cb68a1f4c2a169be404218cf735011ce2
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-17 10:58:31 -0300

    Installed: headlessui/vue, heroicons/vue, tailwindcsss/forms. Feat: created Login, RequestPassword and ResetPassword components

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,10 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+
+
 
 
 const routes = [ // Where routes are defined
@@ -13,7 +17,17 @@ const routes = [ // Where routes are defined
         path: '/login',
         name: 'login',
         component: Login
-    }
+    },
+    {
+        path: '/request-password',
+        name: 'request-password',
+        component: RequestPassword
+    },
+    {
+        path: '/reset-password/token',
+        name: 'reset-password',
+        component: ResetPassword
+    },
 ];
 
 const router = createRouter({
